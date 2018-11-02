46a0944d673932a494f095797a60ba8b54f18294
NadaIhabAhmed <36302376+NadaIhabAhmed@users.noreply.github.com>
2018-11-02 17:23:33 +0200

    Delete deletegrade.php

--- Assignment-1/controllers/deletegrade.php
+++
@@ -1,8 +0,0 @@
-/*<?php
-	header('Content-Type: application/json; charset=utf-8');
-	include_once("../models/grade.php");
-	Database::connect('school', 'root', '');
-	$grade = new grade($_GET['id']);
-	$grade->delete_grade();
-	echo json_encode(['status'=>1]);
-?>
